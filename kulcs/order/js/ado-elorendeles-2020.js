3e0f1d125aab571e2b9b7d99312bd902a037343a
nemethsz <sz.nemeth@hvg.hu>
2020-03-06 12:30:22 +0100

    ado ellenorzes 2020 js added

--- kulcs/order/js/ado-elorendeles-2020.js
+++ kulcs/order/js/ado-elorendeles-2020.js
@@ -1,3 +1,3 @@
 $(function () {
-	$("<section><b>A kiadvány díját csak 2020. április 28. után, az e-mailben küldött díjbekérő megérkezése után kell átutalnia.</b> <br/><br/> Az adatok jóváhagyásához kattintson a "Megrendelem" gombra, majd a következő megrendelés-összegző oldalon hagyja jóvá előrendelését a "Fizetés" gombra való kattintással.</section>" ).insertAfter( "#ExtraInputDataSectionWrapper" );
+	$("<section><b>A kiadvány díját csak 2020. április 28. után, az e-mailben küldött díjbekérő megérkezése után kell átutalnia.</b> <br/><br/> Az adatok jóváhagyásához kattintson a 'Megrendelem' gombra, majd a következő megrendelés-összegző oldalon hagyja jóvá előrendelését a 'Fizetés' gombra való kattintással.</section>" ).insertAfter( "#ExtraInputDataSectionWrapper" );
 });
