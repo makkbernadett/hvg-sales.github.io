4b1a9d990a4e840a184faec95a3c19ed2f30ca32
p.nadas@hvg.hu <p.nadas@hvg.hu>
2020-03-09 10:54:35 +0100

    KNB-2676

---
+++ kulcs/order/js/makbistro-hvg360-gamerule-2020t.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#ExtraConditionSection label").attr({ for: "MakBistroGameRule" });
+    $("#ExtraConditionSection").prependTo("#ExtraInputDataSectionWrapper");
+});
+
+function DoValidationextraInputData() {
+    var validationErrorType = 'none';
+    var data = {
+        succes: true,
+        items: []
+    };
+    var id = "MakBistroGameRule";
+    var input = $("#"+id);
+
+    if (input.hasClass('requied')) {
+        if (input.is(':checked')) {
+            data.items.push({ name: id, val: "accepted" });
+        }
+        else {
+            data.succes = false;
+            showValidationMessage("A megrendelés feladásához el kell fogadnia a játékszabállyal kapcsolatos feltételeket, amellyel részt vehet a sorsolásán.", '#' + id + 'Error', '#' + id);
+        }
+    }
+    return data;
+}
