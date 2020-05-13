8175be3075a0c0d71d5890b12b56f992fc54263a
nemethsz <sz.nemeth@hvg.hu>
2020-05-13 11:37:16 +0200

    extra input js

--- kulcs/order/js/ado-kulonszam-2020t.js
+++ kulcs/order/js/ado-kulonszam-2020t.js
@@ -1,3 +1,3 @@
 $(function () {
-	$("<section><b>A kiadvány díját csak a megjelenés után, az e-mailben kiküldött díjbekérő megérkezését követően kell átutalnia.</b><br/><br/> Az adatok jóváhagyásához kattintson a &quot;Megrendelem&quot; gombra, majd a következő megrendelés-összegző oldalon hagyja jóvá előrendelését a &quot;Fizetés&quot; gombra való kattintással.</section>" ).insertAfter( "#ExtraInputDataSectionWrapper" );
+	$("<section class='extra-input'><b>A kiadvány díját csak a megjelenés után, az e-mailben kiküldött díjbekérő megérkezését követően kell átutalnia.</b><br/><br/> Az adatok jóváhagyásához kattintson a &quot;Megrendelem&quot; gombra, majd a következő megrendelés-összegző oldalon hagyja jóvá előrendelését a &quot;Fizetés&quot; gombra való kattintással.</section>" ).insertAfter( "#ExtraInputDataSectionWrapper" );
 });
