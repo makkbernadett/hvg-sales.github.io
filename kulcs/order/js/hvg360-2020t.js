fdd51bf791e52b3a92e187fb41854f129099f197
Rendes <r.rendes@hvg.hu>
2020-09-01 10:13:35 +0200

    UPDATE: hvg360 js (MGM)

--- kulcs/order/js/hvg360-2020t.js
+++ kulcs/order/js/hvg360-2020t.js
@@ -1,9 +1,17 @@
-$(document).ready(function () {
-	var linkToReplace = $("body a[href='https://bolt.hvg.hu/megrendelesi_feltetelek");
-	linkToReplace.attr('href', 'https://bolt.hvg.hu/megrendelesi_feltetelek_360');
-});
+var dict_promo_translate = {
+    1: "Tisztelt Olvasónk! Az általad kiválasztott promóciós ajánlat felhasználási időtartama lejárt vagy a megadott promóciós kód nem megfelelő. Kérdés esetén fordulj ügyfélszolgálatunkhoz az ugyfelszolgalat@hvg.hu email címen.",
+    2: "Kedves olvasónk! A te HVG profiloddal nem lehetséges az adott kedvezmény igénybevétele. Amennyiben kérdésed van kérjük fordulj ügyfélszolgálatunkhoz az ugyfelszolgalat@hvg.hu email címen.",
+    3: "Kedves olvasónk! A te HVG profiloddal nem lehetséges az adott kedvezmény igénybevétele. Amennyiben kérdésed van kérjük fordulj ügyfélszolgálatunkhoz az ugyfelszolgalat@hvg.hu email címen.",
+    4: "Tisztelt Olvasónk! Az általad kiválasztott promóciós ajánlat felhasználási időtartama lejárt vagy a megadott promóciós kód nem megfelelő. Kérdés esetén fordulj ügyfélszolgálatunkhoz az ugyfelszolgalat@hvg.hu email címen."
+};
+
 $(function () {
-	$('#orderQuantity').hide();
-	$('#orderQuantityError').hide();
-	$('#order-details > label').get(0).remove();
+    var linkToReplace = $("body a[href='https://bolt.hvg.hu/megrendelesi_feltetelek");
+    linkToReplace.attr('href', 'https://bolt.hvg.hu/megrendelesi_feltetelek_360');
+
+    $('#orderQuantity').hide();
+    $('#orderQuantityError').hide();
+    if ($('#order-details > label').get(0)) {
+        $('#order-details > label').get(0).remove();
+    }
 });
